997192861b41502fef8f648fd9c662f170822d1c
vesnicstory <vesnicstory@gmail.com>
2026-08-17 00:51:07 +0800

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -5,9 +5,9 @@
 
 const CONFIG_APP = {
     // 1. Nama Studio Foto Anda (Akan tampil di header dan portal klien)
-    namaVendor: "Portal Client",
+    namaVendor: "Vesnicstory",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
     googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
-    whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
+    whatsappAdmin: "6285145499935" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
